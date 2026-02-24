83948cd8fb1f2374451b9096d9d8ac2731058999
kay1-2-3 <kunalkumar787@gmail.com>
2026-02-24 15:09:28 -0800

    prjects section added

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,4 @@
+import {ArrowUpRight, Github } from "lucide-react";
 
 const projects = [
     {
@@ -7,11 +8,89 @@ const projects = [
         tags: ["React", "TypeScript"],
         link: "#",
         github: "#",
-    }
+    },
+    {
+        title: "Fintech Dashboard",
+        description: "Comprehensive financial analytics",
+        image: "/bg image.webp",
+        tags: ["React", "TypeScript"],
+        link: "#",
+        github: "#",
+    },
+    {
+        title: "Fintech Dashboard",
+        description: "Comprehensive financial analytics",
+        image: "/bg image.webp",
+        tags: ["React", "TypeScript"],
+        link: "#",
+        github: "#",
+    },
+    {
+        title: "Fintech Dashboard",
+        description: "Comprehensive financial analytics",
+        image: "/bg image.webp",
+        tags: ["React", "TypeScript"],
+        link: "#",
+        github: "#",
+    },
 ];
 
 
 
 export const Projects = () => {
-    return <section></section>;
+    return (
+        <section id="projects" className="py-32 relative overflow-hidden">
+            {/*Bg glows */}
+            <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl"></div>
+            <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blue-3xl"></div>
+            <div className="container mx-auto px-6 relative z-10">
+                {/*Section Header */}
+                <div className="text-center mx-auto max-w-3xl mb-16">
+                    <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">
+                        Featured Work
+                    </span>
+                    <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
+                        Projects that{" "}
+                        <span className="font-serif italic font-normal text-white">
+                            make an impact.
+                        </span>
+                    </h2>
+                    <p className="text-muted-foreground animate-fade-in animation-delay-100">
+                        A selection of my recent work, from complex web applications to innovative tools that solve real-world problems.
+                    </p>
+                </div>
+
+                {/*Projects Grid */}
+                <div className="grid md:grid-cols-2 gap-8">
+                    {projects.map((project, idx) => (
+                        <div key={idx} className="group glass rounded-2xl overflow-hidden animate-fade-in md;row-span-1"
+                        style={{ animationDealy: `${(idx + 1) * 100}ms`}}
+                        >
+                            {/*Image*/}
+                            <div className="relative overflow-hidden aspect-video">
+                                <img src={project.image} alt={project.title}
+                                className="w-full h-full object-cover transition-transform duration-1200 group-hover:scale-110"/>
+                                <div
+                                    className="absolute inset-0 bg-gradient-to-t from-card via-card/50 to-transparent opacity-60"
+                                />
+                                {/*Overlay Links*/}
+                                <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transiton-opacity duration-300">
+                                    <a href={project.link} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all">
+                                         <ArrowUpRight className="w-5 h-5"/> 
+                                    </a>
+                                    <a href={project.github} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all">
+                                         <Github className="w-5 h-5"/> 
+                                    </a>
+                                </div>
+                            </div>
+
+                            { /*Content */}
+                            
+                            
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
